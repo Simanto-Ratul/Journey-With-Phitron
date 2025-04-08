1f36638b8241433a1be82cc870cbfcabdff88182
Simanto Saha <sahasimanto.828@gmail.com>
2025-04-08 13:41:57 +0600

    Learning

---
+++ Introduction to Programming/Module 3/exercise1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    if (n % 2 == 0)
+    {
+        printf("You have entered an even number.");
+    }
+    else
+    {
+        printf("You have entered an odd number.");
+    }
+    return 0;
+}
