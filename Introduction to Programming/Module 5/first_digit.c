540357f82cba14a9ece79238927ae3800261512b
Simanto Saha <sahasimanto.828@gmail.com>
2025-04-13 12:36:24 +0600

    Learning

---
+++ Introduction to Programming/Module 5/first_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int x = n / 1000;
+    if (x % 2 == 0)
+    {
+        printf("EVEN");
+    }
+    else
+    {
+        printf("ODD");
+    }
+    return 0;
+}
