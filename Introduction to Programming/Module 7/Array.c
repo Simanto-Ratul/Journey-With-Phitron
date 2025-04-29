07bfd82b77a1a5d333372e1a9a72665f832be246
Simanto Saha <sahasimanto.828@gmail.com>
2025-04-29 11:56:09 +0600

    Learning

---
+++ Introduction to Programming/Module 7/Array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5];
+    arr[1] = 10;
+    arr[2] = 20;
+    arr[3] = 30;
+    arr[4] = 40;
+    printf("%d %d %d %d", arr[1], arr[2], arr[3], arr[4]);
+    return 0;
+}
